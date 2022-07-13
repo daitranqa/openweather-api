eac074d90b53978169ea684e1fed7aa381a0270c
unknown <dai.tranqa@gmail.com>
2022-07-13 22:57:55 +0700

    init project

--- src/base-test.ts
+++
@@ -1,7 +0,0 @@
-export class BaseServices {
-    
-    
-    async function GET(params:requestObject) {
-        return axios.get(params.url, params.options)
-    }
-}
--- src/services/service-base.ts
+++
@@ -1,4 +0,0 @@
-import 
-class ServiceBase {
-
-}
