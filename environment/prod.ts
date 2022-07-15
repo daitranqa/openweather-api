bce9eb8de6112b5f4b6515f4119fbef947bb8ed0
unknown <dai.tranqa@gmail.com>
2022-07-15 23:51:33 +0700

    add new script

--- environment/prod.ts
+++ environment/prod.ts
@@ -1,2 +1,7 @@
 const host: string = "https://api.openweathermap.org";
 const uri: string = "data/2.5/weather"
+
+
+
+
+export default prod;
--- src/test/callCurrentWeather.test.ts
+++ src/test/callCurrentWeather.test.ts
@@ -1,5 +1,11 @@
+import * as request from 'supertest';
+
 describe ('Call Current Weather Feature', () =>{
-    it("Get current weather successfully", () => {
-        
+    it("Get current weather successfully",async () => {
+        let respone = await request('https://api.openweathermap.org').get('data/2.5/weather')
+        .query({"lat":"35",
+        "lon":"139", "appId":"1b9af9f492df041d4ca1a573fa955a62"});
+        console.log(JSON.stringify(request))
+        console.log(JSON.stringify(respone))
     })
 })
